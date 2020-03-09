df59e1f777c2ed29cc337e55883556d8d45df8c1
dominik10pietrzak <d.pietrzak516@op.pl>
2020-03-09 21:30:43 +0100

    Updates

---
+++ precache-manifest.db3950e5494d813669dde25a3016cc2b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bb191459f238fbef6c384774488b83",
+    "url": "/my-introduction/index.html"
+  },
+  {
+    "revision": "7a5107deed7bd98b34fd",
+    "url": "/my-introduction/static/css/main.ccc34646.chunk.css"
+  },
+  {
+    "revision": "6aa4000556f4ca9ffab3",
+    "url": "/my-introduction/static/js/2.4283f21f.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/my-introduction/static/js/2.4283f21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a5107deed7bd98b34fd",
+    "url": "/my-introduction/static/js/main.740665ae.chunk.js"
+  },
+  {
+    "revision": "2130f9205c2254ccb91d",
+    "url": "/my-introduction/static/js/runtime-main.461692a1.js"
+  },
+  {
+    "revision": "023ab602b7405510611a5fc7383d94d4",
+    "url": "/my-introduction/static/media/bg-about-me.023ab602.jpg"
+  },
+  {
+    "revision": "094b60ab48d3cdf514232dd9a2b9b834",
+    "url": "/my-introduction/static/media/bg-alt.094b60ab.jpg"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/my-introduction/static/media/bg-development.00051dbb.jpg"
+  },
+  {
+    "revision": "42288649435d420c471c634b2ca5e398",
+    "url": "/my-introduction/static/media/bg-video-smoke.42288649.mp4"
+  },
+  {
+    "revision": "50eb1bc96681d81397a70c4c0208e68e",
+    "url": "/my-introduction/static/media/gym.50eb1bc9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-introduction/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f88d863e5fa1c9c6dd94edd0c096fe29",
+    "url": "/my-introduction/static/media/my-portrait.f88d863e.png"
+  },
+  {
+    "revision": "ff2d80109b902d091ced37229bfd9c19",
+    "url": "/my-introduction/static/media/phone.ff2d8010.jpg"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/my-introduction/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "1bfa6ade743ed70d16dd799c966146dc",
+    "url": "/my-introduction/static/media/space.1bfa6ade.jpg"
+  }
+]);
